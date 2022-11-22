de8fb126f6777a82c03c5c9881b4880546b6d26a
evanc108 <104491330+evanc108@users.noreply.github.com>
2022-11-22 15:13:18 -0800

    About1

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="projects">
+      <h1 className="projects-title">Projects</h1>
+    </div>
   )
 }
 
