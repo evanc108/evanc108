09ff27e5ccb951d560a732e4a8357a0b29311c36
evanc108 <104491330+evanc108@users.noreply.github.com>
2022-11-21 23:54:48 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Intro from './components/intro/Intro';
+import Navbar from './Navbar';
+import './styles.css';
+import Granim from "granim";
+
+function App() {
+  return (
+    
+    <div className="App">
+      <Navbar />
+      
+      <Intro />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+export default function Navbar() {
+  return (
+    <nav className="n">
+      <a href="/" className="title">
+      </a>
+      <ul>
+        <li>
+          <a href="/Home">Home</a>
+        </li>
+        <li>
+          <a href="/About">About</a>
+        </li>
+        <li>
+          <a href="/Contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
